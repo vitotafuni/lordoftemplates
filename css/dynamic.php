26f14d0fbc64bb9efebd32ffe6e6521defbbe32f
Vito Tafuni <vitotafuni@gmail.com>
2010-11-23 12:23:01 +0100

    added a dynamic php css

---
+++ css/dynamic.php
@@ -0,0 +1,21 @@
+<?php
+	/*
+	* The dynamic css gives you the possibility to play with css
+	* using some server-side calculated variables
+	*/
+	function getRGB($baseRGB){
+      	$baseRGB = round((216*$baseRGB)/255)%216; // only 216 web safe colors
+		$RGB = array('00','33','66','99','CC','FF');
+    	$R = $RGB[$baseRGB % 6];
+		$G = $RGB[round($baseRGB/36)];
+      	$B = $RGB[round($baseRGB/6)%6];
+      	return '#'.$R.$G.$B;
+	}
+	
+	$ip=explode('.',$_SERVER['REMOTE_ADDR']);
+	
+	$color = getRGB($ip[3]);
+
+?>
+
+a { color:<?php echo $color; ?>; }
