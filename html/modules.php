f051c8787e242eecb27446ace0477324ac47091e
Vito Tafuni <vitotafuni@gmail.com>
2011-03-30 18:47:45 +0200

    the file is not needed but leaved for feature improvements

--- html/modules.php
+++ html/modules.php
@@ -9,70 +9,11 @@
 // no direct access
 defined('_JEXEC') or die;
 
-/*
- * none (output raw module content)
- */
-function modChrome_none($module, &$params, &$attribs)
-{
-	echo $module->content;
-}
-
-/*
- * Module chrome that wraps the module in a table
- */
-function modChrome_table($module, &$params, &$attribs)
-{ ?>
-	<table cellpadding="0" cellspacing="0" class="moduletable<?php echo htmlspecialchars($params->get('moduleclass_sfx')); ?>">
-	<?php if ($module->showtitle != 0) : ?>
-		<tr>
-			<th>
-				<?php echo $module->title; ?>
-			</th>
-		</tr>
-	<?php endif; ?>
-		<tr>
-			<td>
-				<?php echo $module->content; ?>
-			</td>
-		</tr>
-		</table>
-	<?php
-}
-
-/*
- * Module chrome that wraps the tabled module output in a <td> tag of another table
- */
-function modChrome_horz($module, &$params, &$attribs)
-{ ?>
-	<table cellspacing="1" cellpadding="0" width="100%">
-		<tr>
-			<td>
-				<?php modChrome_table($module, $params, $attribs); ?>
-			</td>
-		</tr>
-	</table>
-	<?php
-}
-
-/*
- * xhtml (divs and font headder tags)
- */
-function modChrome_xhtml($module, &$params, &$attribs)
-{
-	if (!empty ($module->content)) : ?>
-		<div class="moduletable<?php echo htmlspecialchars($params->get('moduleclass_sfx')); ?>">
-		<?php if ($module->showtitle != 0) : ?>
-			<h3><?php echo $module->title; ?></h3>
-		<?php endif; ?>
-			<?php echo $module->content; ?>
-		</div>
-	<?php endif;
-}
 
 /*
  * Module chrome that allows for rounded corners by wrapping in nested div tags
  */
-function modChrome_rounded($module, &$params, &$attribs)
+/*function modChrome_rounded($module, &$params, &$attribs)
 { ?>
 		<div class="module<?php echo htmlspecialchars($params->get('moduleclass_sfx')); ?>">
 			<div>
@@ -91,7 +32,7 @@ function modChrome_rounded($module, &$params, &$attribs)
 
 /*
  * Module chrome that add preview information to the module
- */
+ *//*
 function modChrome_outline($module, &$params, &$attribs)
 {
 	static $css=false;
@@ -128,5 +69,5 @@ function modChrome_outline($module, &$params, &$attribs)
 		</div>
 	</div>
 	<?php
-}
+}*/
 ?>
